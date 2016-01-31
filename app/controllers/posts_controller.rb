c2ae314d042577e1db21fb1dd7e1cba0ac0e54cd
ohsofoxy273 <Matthew.F.Fox@gmail.com>
2016-01-31 17:49:14 -0600

    refactor posts controller with before_action method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy] 
 
   def index
     @posts = Post.all
@@ -9,26 +10,25 @@ class PostsController < ApplicationController
   end
 
   def create
-    Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     redirect_to(post_path(@post))
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
@@ -37,5 +37,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
